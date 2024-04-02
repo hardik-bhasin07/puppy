3ea8fdde5f90225e1144cc3f98be0103040cc208
Hardik Bhasin <hardikbhasin11@gmail.com>
2024-04-02 20:47:33 +0530

    third

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hardik-bhasin07/puppy
+
+go 1.22.1
+
+require github.com/hardik-bhasin07/dog v0.0.0-20240402150716-a79ace0a4114 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import (
+	"github.com/hardik-bhasin07/dog"
+)
+
 func Bark() string {
 	return "whoof!"
 }
 
 func Barks() string {
 	return "whoof whoof whoof"
+}
+func GrownBarks() string {
+	return dog.WhenGrownUp(Barks())
+}
+func GrownBark() string {
+	return dog.WhenGrownUp(Bark())
 }
